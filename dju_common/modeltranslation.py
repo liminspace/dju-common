1a6b4d3e9d97a1c820a24b366f83a6cca3f60492
liminspace <igormeln@gmail.com>
2016-01-07 22:13:57 +0200

    Add modeltranslation tool

---
+++ dju_common/modeltranslation.py
@@ -0,0 +1,18 @@
+from modeltranslation.settings import AVAILABLE_LANGUAGES
+
+
+def mt_fields(*fields):
+    """
+    Returns list of fields for multilanguage fields of model.
+    Examples:
+        print mt_fields('name', 'desc')
+        ['name', 'name_en', 'name_uk', 'desc', 'desc_en', 'desc_uk']
+
+        MyModel.objects.only(*mt_fields('name', 'desc', 'content'))
+    """
+    fl = []
+    for field in fields:
+        fl.append(field)
+        for lang in AVAILABLE_LANGUAGES:
+            fl.append('{}_{}'.format(field, lang))
+    return fl
