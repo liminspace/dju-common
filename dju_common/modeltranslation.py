68c56f52cb9ea168b9d0678e7742abcc858bbda9
liminspace <igormeln@gmail.com>
2016-07-10 03:25:57 +0300

    Update func mt_fields. Update to version 0.2.6

--- dju_common/modeltranslation.py
+++ dju_common/modeltranslation.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
-from modeltranslation.settings import AVAILABLE_LANGUAGES
+from modeltranslation.settings import AVAILABLE_LANGUAGES, DEFAULT_LANGUAGE
 
 
-def mt_fields(*fields):
+def mt_fields(nomaster__=False, onlydefaultlang__=False, *fields):
     """
     Returns list of fields for multilanguage fields of model.
     Examples:
@@ -10,10 +10,23 @@ def mt_fields(*fields):
         ['name', 'name_en', 'name_uk', 'desc', 'desc_en', 'desc_uk']
 
         MyModel.objects.only(*mt_fields('name', 'desc', 'content'))
+
+    If nomaster__ then master field will not be append.
+    F.e.: ['name_en', 'name_uk'] -- without master 'name'.
+
+    If onlydefaultlang__ then wiil be search only default language:
+    F.e.: ['name', 'name_en'] -- without additional 'name_uk'.
+
+    If nomaster__ and onlydefaultlang__ then will be use both rulses.
+    F.e.: ['name_en'] -- without master 'name' and additional 'name_uk'.
     """
     fl = []
     for field in fields:
-        fl.append(field)
-        for lang in AVAILABLE_LANGUAGES:
-            fl.append('{}_{}'.format(field, lang))
+        if not nomaster__:
+            fl.append(field)
+        if onlydefaultlang__:
+            fl.append('{}_{}'.format(field, DEFAULT_LANGUAGE))
+        else:
+            for lang in AVAILABLE_LANGUAGES:
+                fl.append('{}_{}'.format(field, lang))
     return fl
