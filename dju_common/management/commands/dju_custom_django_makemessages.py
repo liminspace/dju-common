59aeb95d657e50d793903304d4febc92a511a35f
liminspace <igormeln@gmail.com>
2016-09-15 23:14:25 +0300

    Update makemessages commans. Release v.0.3.3

--- dju_common/management/commands/dju_custom_django_makemessages.py
+++ dju_common/management/commands/dju_custom_django_makemessages.py
@@ -1,5 +1,5 @@
 import os
-
+from django.core.management import CommandError
 from django.core.management.commands import makemessages
 
 
@@ -9,13 +9,18 @@ class Command(makemessages.Command):
         parser.add_argument('--add-source-dir', action='append', dest='additional_source_dirs',
                             default=[], metavar='DIR',
                             help='Additional source directory. For example: "/path/to/src:/path/to/locale"')
-        parser.add_argument('--omit-header', action='store_true', dest='omit_header', default=False,
-                            help="Don't write header with 'msgid \"\"' entry.")
+        parser.add_argument('--no-update-header', action='store_true', dest='no_update_header', default=False,
+                            help="Don't update POT-Creation-Date in headers.")
 
     def handle(self, *args, **options):
         self.additional_source_dirs = options['additional_source_dirs']
-        if options['omit_header']:
-            self.xgettext_options = makemessages.Command.xgettext_options[:] + ['--omit-header']
+        self.no_update_header = options['no_update_header']
+        if self.no_update_header:
+            try:
+                import polib
+            except ImportError:
+                raise CommandError('Please, install "polib", just run "pip install polib".')
+            self.polib = polib
         super(Command, self).handle(*args, **options)
 
     def find_files(self, root):
@@ -32,3 +37,16 @@ class Command(makemessages.Command):
                     add_file.locale_dir = locale_dir
             files.extend(add_files)
         return sorted(files)
+
+    def write_po_file(self, potfile, locale):
+        pofile_fn = os.path.join(os.path.dirname(potfile), locale, 'LC_MESSAGES', '{}.po'.format(self.domain))
+        old_metadata = {}
+        if self.no_update_header and os.path.isfile(pofile_fn) :
+            po = self.polib.pofile(pofile_fn)
+            old_metadata = po.metadata
+            del po
+        super(Command, self).write_po_file(potfile, locale)
+        if old_metadata:
+            po = self.polib.pofile(pofile_fn)
+            po.metadata['POT-Creation-Date'] = old_metadata['POT-Creation-Date']
+            po.save()
--- dju_common/management/commands/dju_makemessages.py
+++ dju_common/management/commands/dju_makemessages.py
@@ -26,8 +26,8 @@ class Command(BaseCommand):
         parser.add_argument('--extension', '-e', action='append', dest='extensions', default=None,
                             help='The file extension(s) to examine (default: "html,txt,rml", or "js" if the domain is '
                                  '"djangojs"). Separate multiple extensions with commas, or use -e multiple times.')
-        parser.add_argument('--omit-header', action='store_true', dest='omit_header', default=False,
-                            help="Don't write header with 'msgid \"\"' entry.")
+        parser.add_argument('--no-update-header', action='store_true', dest='no_update_header', default=False,
+                            help="Don't update POT-Creation-Date in headers.")
 
     def _get_paths_of_apps(self, apps):
         result = []
@@ -81,8 +81,8 @@ class Command(BaseCommand):
                 command_base.extend(['-l', lang])
         else:
             command_base.append('-a')
-        if options['omit_header']:
-            command_base.append('--omit-header')
+        if options['no_update_header']:
+            command_base.append('--no-update-header')
         if not options['domain']:
             options['domain'] = ['django', 'djangojs']
         for path in paths:
